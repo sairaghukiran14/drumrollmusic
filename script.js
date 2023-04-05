9874338b1654257236bd8252789f2c8cf3da5a5b
Avula Sai Raghu Kiran <106566627+sairaghukiran14@users.noreply.github.com>
2023-04-05 18:55:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("keydown", (e) => {
+  let t = e.key.toUpperCase();
+  var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+  var mus = new Audio("music/" + t + ".mp3");
+  console.log(mus);
+  mus.play();
+});
+
+let timer = document.getElementsByClassName("timer");
+setTimeout(livetime, 1000);
+function livetime() {
+  setInterval(function () {
+    let t = new Date();
+    timer[0].innerHTML =
+      t.getHours() + "hr " + t.getMinutes() + "min " + t.getSeconds() + "sec";
+  }, 1000);
+}
+
+window.addEventListener("onclick", playmu);
+
+function playmu(t1) {
+  var mu = new Audio("music/" + t1.toUpperCase() + ".mp3");
+  mu.play();
+}
