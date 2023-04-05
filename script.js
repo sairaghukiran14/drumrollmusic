4e1834905dbaf5d8b05b60e85ffbca325bd5fcb4
Avula Sai Raghu Kiran <106566627+sairaghukiran14@users.noreply.github.com>
2023-04-05 20:49:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("keydown", (e) => {
   let t = e.key.toUpperCase();
-//   var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-//   document.body.style.backgroundColor = randomColor;
+   var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+ document.body.style.backgroundColor = randomColor;
   var mus = new Audio(t + ".mp3");
   console.log(mus);
   mus.play();
